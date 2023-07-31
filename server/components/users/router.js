80eabbce322826188318364845a1695f53a0d45c
juancodev <montillasanchezjuancarlos@gmail.com>
2023-07-31 10:01:47 -0400

    environment on window

--- server/components/users/router.js
+++ server/components/users/router.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const controller = require('./controller');
 const response = require('../../response/index');
 
+router.get('/', (req, res) => {
+  controller.getUsers(req.params)
+    .then((user) => response.success(req, res, user, 200))
+    .catch((error) => response.error(req, res, 'Internal Error', 500, error));
+});
+
 router.post('/', (req, res) => {
   controller.addUser(req.body)
     .then((user) => response.success(req, res, user, 201))
--- server/firebase/firebase.config.js
+++ server/firebase/firebase.config.js
@@ -4,9 +4,16 @@ const {
   applicationDefault
 } = require('firebase-admin/app');
 
-
 const app = admin.initializeApp({
-  credential: applicationDefault()
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
+
+// const app = admin.initializeApp({
+//   credential: applicationDefault()
+// });
 
 module.exports = app
